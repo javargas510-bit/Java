dfb0cddc0500abfc0898b395c7205f10924cf558
javargas510-bit <javargas209@gmail.com>
2021-08-19 00:06:38 -0700

    Adding another folder for examples

---
+++ Pythagorean Theorem/PythagoreanTest.java
@@ -0,0 +1,9 @@
+
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean triangle = new Pythagorean();
+        double getAnswer = triangle.calculateHypotenuse(3,12);
+        
+        System.out.println(getAnswer);
+    }
+}
